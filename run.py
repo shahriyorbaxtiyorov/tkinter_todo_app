cb253920d32ece6c4f2197d59b8367f5f2ffe2c0
shahr <shahriyorbaxtiyorov007@gmail.com>
2022-07-29 20:49:48 +0500

    create packages

---
+++ run.py
@@ -0,0 +1,4 @@
+from ui.main import App
+
+if __name__ == '__main__':
+    App().run()
---
+++ ui/main.py
@@ -0,0 +1,11 @@
+from tkinter import Tk
+from tkinter.ttk import Label, Button, Entry
+
+
+# Main Window
+class App(Tk):
+    def __init__(self):
+        super().__init__()
+
+    def run(self):
+        self.mainloop()
